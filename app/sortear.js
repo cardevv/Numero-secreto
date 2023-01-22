4ed537864a211657bcf6efeecd945bb6bf6f7c3b
cardevv <113392365+cardevv@users.noreply.github.com>
2023-01-22 18:40:29 -0300

    .

--- app/sortear.js
+++ app/sortear.js
@@ -1,11 +1,25 @@
+const menorValor = 1;
+const maiorValor = 100;
+
+
 const NumeroSecreto = sorteia();
 
 
+
+
 function sorteia () {
-    return parseInt(Math.random()*100)
+    return parseInt(Math.random()* maiorValor + 1 )
 
     
 
 }
 
 console.log('Número secreto:', NumeroSecreto)
+
+
+const elementoMaiorValor = document.querySelector('#maior-valor')
+elementoMaiorValor.innerHTML = maiorValor
+
+
+const elementoMenorValor = document.querySelector('#menor-valor')
+elementoMenorValor.innerHTML = menorValor
